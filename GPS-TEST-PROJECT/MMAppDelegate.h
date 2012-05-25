9024b2fe4302802fa7020ef9d40cec470adb3cca
maiamarc <marc@maiamedia.com>
2012-05-25 12:07:29 +0200

    Initial Commit

---
+++ GPS-TEST-PROJECT/MMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MMAppDelegate.h
+//  GPS-TEST-PROJECT
+//
+//  Created by MARC MONTEYS on 25/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GPS-TEST-PROJECT/MMViewController.m
@@ -0,0 +1,34 @@
+//
+//  MMViewController.m
+//  GPS-TEST-PROJECT
+//
+//  Created by MARC MONTEYS on 25/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MMViewController.h"
+
+@interface MMViewController ()
+
+@end
+
+@implementation MMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
